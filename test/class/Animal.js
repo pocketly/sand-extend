1c3c69413adf2e3ea63c58fd31912dffb0d85586
Adam Jaso <ajaso@sazze.com>
2014-12-11 14:45:31 -0800

    added support for Class style extend of built-in objects

--- test/class/Animal.js
+++ test/class/Animal.js
@@ -1,6 +1,7 @@
 var Class = require('../../').Class;
+var EventEmitter = require('events').EventEmitter;
 
-var Animal = Class.extend({
+var Animal = Class.extend(EventEmitter, {
   construct: function(name) {
     this.name = name || 'No Name';
   },
